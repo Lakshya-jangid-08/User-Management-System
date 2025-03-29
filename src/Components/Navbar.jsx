ccdf9091e1ed98e8438619d1207381c17358c892
lakshya jangid IIITV <202351075@iiitvadodara.ac.in>
2025-03-29 16:15:22 +0530

    fix navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
 
   return (
     <div className='abosulte w-full h-fit top-0 bg-gradient-to-b from-cyan-300 to-cyan-200  border-b-2 border-black '>
-        <div className='flex justify-between items-center px-3 sm:px-5 md:px-8 py-2 sm:py-4 md:py-6'>
+        <div className='flex justify-between items-center px-3 sm:px-5 md:px-8 py-4 sm:py-6 md:py-8'>
             <div className='text-lg sm:text-xl md:text-2xl font-bold'>User Management System</div>
             <div className='flex items-center gap-5'>
               <h1 className='font-semibold text-base sm:text-lg md:text-xl'>Signout</h1>
