0c5986ab81f78ca007ca6288d3969fccc4e0c5a2
lakshya jangid IIITV <202351075@iiitvadodara.ac.in>
2025-03-29 18:28:08 +0530

    fb update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                 <h1 className='text-center text-xl font-semibold'>Follow us on:</h1>
                 <div className='grid grid-cols-2 gap-4 mt-2'>
                     <a href="https://www.instagram.com/the.lakshya.jangid/" className='flex justify-center items-center gap-2'>  <FaInstagram className='cursor-pointer' size={24}/> Instagram </a>
-                    <a href="#" className='flex justify-center items-center gap-2'>  <FaFacebook className='cursor-pointer' size={24}/> Facebook </a>
+                    <a href="https://www.facebook.com/share/12H6Apbj7gR/" className='flex justify-center items-center gap-2'>  <FaFacebook className='cursor-pointer' size={24}/> Facebook </a>
                     <a href="#" className='flex justify-center items-center gap-2'>  <FaTwitter className='cursor-pointer' size={24}/> Twitter</a>
                     <a href="https://www.bing.com/ck/a?!&&p=8ca4a6029b38f1628dae0b58db868e4566c203ab9dcccdf41b409dec2f1bd4e6JmltdHM9MTc0MzIwNjQwMA&ptn=3&ver=2&hsh=4&fclid=1e527f80-3a37-63c1-13cb-6af13ba66202&psq=lakshya+jangid+iiitv&u=a1aHR0cHM6Ly9pbi5saW5rZWRpbi5jb20vaW4vbGFrc2h5YS1qYW5naWQtMDU2MjUwMmEy&ntb=1" className='flex justify-center items-center gap-2'>  <FaLinkedin className='cursor-pointer' size={24}/> LinkedIN</a>
                  </div>
